7b597861488ddc7a94808054022bcc63d4906824
danyill <danyill@users.noreply.github.com>
2025-01-30 23:17:34 +0000

    Deploying to gh-pages from @ danyill/oscd-snapshot@ba47c55f9c2824b9bb24c2048165a1d4e22a0c4e 🚀

--- oscd-snapshot.d.ts
+++ oscd-snapshot.d.ts
@@ -1,9 +1,14 @@
-import { LitElement } from 'lit';
+import { LitElement, TemplateResult } from 'lit';
+import '@material/mwc-snackbar';
+import type { Snackbar } from '@material/mwc-snackbar';
 /** An editor [[`plugin`]] to create a snapshot of the currently open SCL file */
 export default class SnapshotPlugin extends LitElement {
     /** The document being edited as provided to plugins by [[`OpenSCD`]]. */
     doc: XMLDocument;
     docName: string;
+    userMessage: string;
     docs: Record<string, XMLDocument>;
+    userMessageUI?: Snackbar;
     run(): Promise<void>;
+    render(): TemplateResult;
 }
